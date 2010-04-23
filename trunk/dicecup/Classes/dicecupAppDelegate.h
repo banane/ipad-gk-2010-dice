8c16370660470a2c965a8d5f041fea035aa565dc
banane <banane@9445b5f4-ee35-603c-c532-b89848e68f17>
2010-04-23 18:21:39 +0000

    sound added

--- trunk/dicecup/Classes/dicecupAppDelegate.h
+++ trunk/dicecup/Classes/dicecupAppDelegate.h
@@ -7,12 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 #import <GameKit/GameKit.h>
 
 @class dicecupViewController;
 
 @interface dicecupAppDelegate : NSObject <UIApplicationDelegate, GKPeerPickerControllerDelegate, GKSessionDelegate, UIAlertViewDelegate, UIAccelerometerDelegate> {
     UIWindow		*window;
+	UIViewController *viewController;
     GKSession		*myGkSession;
     NSMutableArray  *myPeers;
 	NSString		*iPadPeerID;
@@ -21,19 +23,24 @@
 	BOOL shakeable;
 
 	NSMutableArray	*diceValues;
+	
+	SystemSoundID mysound;
 
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UIViewController *viewController;
 @property (nonatomic, retain) GKSession	 *myGkSession;
 @property (nonatomic, retain) NSMutableArray *myPeers;
 @property (nonatomic, retain) UIAccelerometer *accelerometer;
 @property (nonatomic) BOOL	shakeable;
 @property (nonatomic, retain) NSMutableArray *diceValues;
 @property (nonatomic, retain) NSString *iPadPeerID;
+@property (nonatomic) SystemSoundID mysound;
 
 -(void)startPicker;
 -(void)rollDice;
+- (void) playSound;
 
 
 @end
